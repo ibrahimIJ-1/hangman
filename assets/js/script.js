839afc216eb806dd84a9d12d89d6068667dd6556
zaki-kh <zakikhanji@gmail.com>
2023-04-19 17:18:58 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,8 +19,8 @@ let selectedWord;
 let selectedhint;
 let randomNumber;
 
-displayWord();
-
+createWordsArray();
+// getting the json file
 function getJsonFile() {
   return fetch("./../assets/files/JSON.json")
     .then((response) => response.json())
@@ -28,22 +28,25 @@ function getJsonFile() {
       Object.assign(wordsObj, data);
     });
 }
-
+// filling the json object into two arrays (word and hint)
 async function createWordsArray() {
   await getJsonFile();
   for (let x in wordsObj) {
     words.push(wordsObj[x].word);
     hint.push(wordsObj[x].hint);
   }
+  pickWordHint();
+  displayWord();
+}
+// generate random word and its hint and display them
+function pickWordHint() {
   randomNumber = Math.floor(Math.random() * words.length);
   selectedWord = words[randomNumber];
   selectedhint = hint[randomNumber];
   displayHint.innerHTML = selectedhint;
 }
-
 // Show hidden word
-async function displayWord() {
-  await createWordsArray();
+function displayWord() {
   wordEl.innerHTML = `
     ${selectedWord
       .split("")
@@ -56,9 +59,7 @@ async function displayWord() {
       )
       .join("")}
   `;
-
   const innerWord = wordEl.innerText.replace(/[ \n]/g, "");
-
   if (innerWord === selectedWord) {
     finalMessage.innerText = "Congratulations! You won! 😃";
     finalMessageRevealWord.innerText = "";
@@ -67,7 +68,6 @@ async function displayWord() {
     playable = false;
   }
 }
-
 // Update the wrong letters
 function updateWrongLettersEl() {
   // Display wrong letters
@@ -75,47 +75,38 @@ function updateWrongLettersEl() {
     ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
     ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
   `;
-
   // Display parts
   figureParts.forEach((part, index) => {
     const errors = wrongLetters.length;
-
     if (index < errors) {
       part.style.display = "block";
     } else {
       part.style.display = "none";
     }
   });
-
   // Check if lost
   if (wrongLetters.length === figureParts.length) {
     finalMessage.innerText = "Unfortunately you lost. 😕";
     finalMessageRevealWord.innerText = `...the word was: ${selectedWord}`;
     popup.style.display = "flex";
-
     playable = false;
   }
 }
-
 // Show notification
 function showNotification() {
   notification.classList.add("show");
-
   setTimeout(() => {
     notification.classList.remove("show");
   }, 2000);
 }
-
 // Keydown letter press
 window.addEventListener("keydown", (e) => {
   if (playable) {
     if (e.keyCode >= 65 && e.keyCode <= 90) {
       const letter = e.key.toLowerCase();
-
       if (selectedWord.includes(letter)) {
         if (!correctLetters.includes(letter)) {
           correctLetters.push(letter);
-
           displayWord();
         } else {
           showNotification();
@@ -123,7 +114,6 @@ window.addEventListener("keydown", (e) => {
       } else {
         if (!wrongLetters.includes(letter)) {
           wrongLetters.push(letter);
-
           updateWrongLettersEl();
         } else {
           showNotification();
@@ -140,14 +130,8 @@ playAgainBtn.addEventListener("click", () => {
   //  Empty arrays
   correctLetters.splice(0);
   wrongLetters.splice(0);
-
-  randomNumber = Math.floor(Math.random() * words.length);
-  selectedWord = words[randomNumber];
-  selectedhint = hint[randomNumber];
-  displayHint.innerHTML = selectedhint;
+  pickWordHint();
   displayWord();
-
   updateWrongLettersEl();
-
   popup.style.display = "none";
 });
